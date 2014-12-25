7227355c58dc0a88d2eca7913941ba935d910ab5
skeksify <ben.haran@gmail.com>
2014-12-25 07:01:38 +0200

    Hexagon First Final Touchup
    Added proper levels, Captions, Colors, Draw Ability Set (Draw Char)

--- views/resources/js/hexagon.js
+++ views/resources/js/hexagon.js
@@ -1,54 +1,115 @@
 /**
  * Created by Skeksify on 12/25/14.
  */
+var my_hex;
 
 function draw_hexagon() {
     var create_character_canvas = document.getElementById("hexagon");
     var context = create_character_canvas.getContext('2d');
+    var rad = 240;
+    var levels = 7;
+    var distance = rad / levels;
+    var vertices = {
+        STR: function (x, y, rad) {
+            return {x: x, y: y - rad};
+        },
+        DEX: function (x, y, rad) {
+            return {x: x + rad, y: y - rad / 2};
+        },
+        CON: function (x, y, rad) {
+            return {x: x + rad, y: y + rad / 2};
+        },
+        INT: function (x, y, rad) {
+            return {x: x, y: y + rad};
+        },
+        WIS: function (x, y, rad) {
+            return {x: x - rad, y: y + rad / 2};
+        },
+        CHA: function (x, y, rad) {
+            return {x: x - rad, y: y - rad / 2};
+        }
+    }
+    var abilityCaptions = {
+        STR: {x: -10, y: -10},
+        DEX: {x: 5, y: -5},
+        CON: {x: 5, y: 5},
+        INT: {x: -15, y: 20},
+        WIS: {x: -45, y: 5},
+        CHA: {x: -45, y: -5}
+    }
+    var firstSettings;
 
-    function hex(context, params) {
-        var rad = params.rad;
+    function hex_grid(context, params) {
         var x = params.cx;
         var y = params.cy;
-        context.beginPath();
+        if (1) { //Diagonals
+            context.strokeStyle = '#333';
+            context.beginPath();
+            context.moveTo(vertices.STR(x, y, rad).x, vertices.STR(x, y, rad).y);
+            context.lineTo(vertices.INT(x, y, rad).x, vertices.INT(x, y, rad).y);
 
-        context.moveTo(x + rad, y - rad / 2); //Right Upper
-        context.lineTo(x, x - rad); //Upper
-        context.lineTo(x - rad, y - rad / 2); //Left Upper
-        context.lineTo(x - rad, y + rad / 2); //Left Downer
-        context.lineTo(x, y + rad); //Downer
-        context.lineTo(x + rad, y + rad / 2); //Right Downer
+            context.moveTo(vertices.CHA(x, y, rad).x, vertices.CHA(x, y, rad).y);
+            context.lineTo(vertices.CON(x, y, rad).x, vertices.CON(x, y, rad).y);
 
-        context.closePath();
-        context.stroke();
+            context.moveTo(vertices.WIS(x, y, rad).x, vertices.WIS(x, y, rad).y);
+            context.lineTo(vertices.DEX(x, y, rad).x, vertices.DEX(x, y, rad).y);
+            context.stroke();
+        }
+        for (var i = 0; i <= levels; i++) {
+            context.strokeStyle = i == (levels - 2) ? '#ddd' : '#000';
+            context.beginPath();
+            context.moveTo(vertices.STR(x, y, rad - i * distance).x, vertices.STR(x, y, rad - i * distance).y); // Start with STR
+            for (var Ability in vertices) {
+                var nextPoint = vertices[Ability](x, y, rad - i * distance);
+                context.lineTo(nextPoint.x, nextPoint.y);
+                if (!i) {
+                    context.font = 'italic 16px Verdana';
+                    context.fillStyle = '#eee';
+                    context.fillText(Ability, nextPoint.x + abilityCaptions[Ability].x, nextPoint.y + abilityCaptions[Ability].y);
+                } else if(Ability=='DEX'){
+                    context.font = 'italic 16px Arial';
+                    context.fillStyle = '#eee';
+                    var mod = levels-i-2;
+                    context.fillText((mod>-1?'+':'')+mod, nextPoint.x-4, nextPoint.y);
+                }
+            }
+            context.closePath();
+            context.stroke();
+        }
     }
 
-    function hex_diagonals(context, params){
-        var rad = params.rad;
-        var x = params.cx;
-        var y = params.cy;
+    this.init = function (settings) {
+        // Store first settings for later redraws of the grid
+        if (settings) // First Init, regular
+            firstSettings = settings;
+        else // Not first, clear context
+            context.clearRect(0, 0, firstSettings.width, firstSettings.height);
+        hex_grid(context, {cx: firstSettings.width / 2, cy: firstSettings.height / 2}); // Rad here defines the outer hex
+    }
 
+    this.draw_set = function (set) {
+        this.init();
         context.beginPath();
-        context.moveTo(x, y - rad); //Upper
-        context.lineTo(x, y + rad); //Downer
-        context.stroke();
-
-        context.moveTo(x  - rad, y - rad/2); //Left Upper
-        context.lineTo(x +rad, y +rad/2); //Right Downer
-        context.stroke();
-
-        context.moveTo(x  - rad, y + rad/2); //Left Upper
-        context.lineTo(x +rad, y -rad/2); //Right Downer
+        context.strokeStyle = '#ff0';
+        var x = firstSettings.width / 2;
+        var y = firstSettings.height / 2;
+        var start = {
+            x: vertices.STR(x, y, (set.STR * distance) + (distance * 2)).x,
+            y: vertices.STR(x, y, (set.STR * distance) + (distance * 2)).y
+        };
+        context.moveTo(start.x, start.y);
+        for (var Ability in set) {
+            var nextPoint = vertices[Ability](x, y, (set[Ability] * distance) + (distance * 2));
+            context.lineTo(nextPoint.x, nextPoint.y);
+        }
+        context.lineTo(start.x, start.y);
         context.stroke();
 
     }
 
-    hex(context, {cx: 200, cy: 200, rad: 50});
-    hex(context, {cx: 200, cy: 200, rad: 100});
-    hex(context, {cx: 200, cy: 200, rad: 150});
-    hex_diagonals(context, {cx: 200, cy: 200, rad: 150});
-} 
+}
 
 $(document).ready(function () {
-    draw_hexagon();
+    my_hex = new draw_hexagon();
+    my_hex.init({width: 600, height: 600});
 })
